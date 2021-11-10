ff6f2041553b57af0ae4003f399ede3dafe6469d
evanderford.f19 <evanderford.f19@warren-wilson.edu>
2021-11-10 15:50:55 -0500

    #class 11/121

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import csv
+with open('data/volcanoes.tsv') as csvfile:
+    dialect = csv.Sniffer().sniff(csvfile.read())
+    csvfile.seek(0)
+    reader= csv.DictReader(csvfile, dialect=dialect)
+    rows = []
+    for row in reader:
+        if not (row["Volcano Name"]=="Unnamed" or row["Last Known Eruption"]=="?"):
+            rows.append(row)
+            print(row)
+
+
+
 MAPNAME='data/map1.html'
 if __name__ == '__main__':
     import folium
---
+++ mapname.py
@@ -0,0 +1,8 @@
+import csv
+with open('data/volcanoes.tsv', newline='') as tsvfile
+MAPNAME='data/map1.html'
+if __name__ == '__main__':
+    import folium
+    dir(folium)
+    map = folium.Map(location=(35.61,-82.44), zoom_start = 10)
+    map.save(MAPNAME)
