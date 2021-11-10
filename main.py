ce325f8fbc3f203464af140d295e0b2cf5d54bb9
evanderford.f19 <evanderford.f19@warren-wilson.edu>
2021-11-10 15:50:55 -0500

    #class 11/121

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import csv
+fv= open("data/volcanoes.tsv","r")
+with open('data/volcanoes.tsv') as csvfile:
+    dialect = csv.Sniffer().sniff(csvfile.read())
+    csvfile.seek(0)
+    reader= csv.DictReader(csvfile, dialect=dialect)
+    rows = []
+    for row in reader:
+        print(rows.append(row))
+
 MAPNAME='data/map1.html'
 if __name__ == '__main__':
     import folium
